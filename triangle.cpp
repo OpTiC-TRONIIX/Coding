d22bc088654926cb69e394427464a97b52464370
OpTiC-TRONIIX <54913817+OpTiC-TRONIIX@users.noreply.github.com>
2021-03-02 00:19:33 +0300

    Add files via upload

---
+++ triangle.cpp
@@ -0,0 +1,39 @@
+// triangle.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\"Hello Triangle!\"" << endl;
+    cout << "* * * *" << endl;
+    cout << " * * * " << endl;
+    cout << "  * *  " << endl;
+    cout << "   *   " << endl;
+    cout << "  * *  " << endl;
+    cout << " * * * " << endl;
+    cout << "* * * *" << endl;
+
+    /*
+    PIC 10A
+    Homework 1: triangle.cpp
+    Purpose: Outputs the phrase "Hello Triangle!" along with an hourglass made of stars.
+    Author: Amaan Hassanali
+    Date: 01/07/2020
+    */
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
